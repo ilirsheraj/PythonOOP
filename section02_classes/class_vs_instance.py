3f28417faf928500e2576615a394f0edd2d96629
Like <sherajilir@gmail.com>
2022-11-16 23:59:49 +0100

    Add files via upload

---
+++ section02_classes/class_vs_instance.py
@@ -0,0 +1,27 @@
+class MercedesBenz:
+	doors = 4
+	wheels = 4
+	model = "G"
+
+	def __init__(self, color="black"):
+		self.color = color
+
+	def drive(self):
+		print(f"A Mercedes is driving. It is {self}/n")
+
+	# Convert it into static method
+	@staticmethod
+	def auto_drive():
+		return "Auto-driving for now..."
+
+	@classmethod
+	def create_lease(cls):
+		print(f"A lease for {cls} will be created")
+
+
+print(MercedesBenz.__dict__)
+print(type(MercedesBenz.__dict__))
+
+m1 = MercedesBenz("lavender")
+m1.__dict__["horse_power"] = 290
+print(m1.__dict__)
