5ba6813058dbd594a3e98bc44d6fb5e91a3db69e
Like <sherajilir@gmail.com>
2022-12-22 21:01:12 +0100

    Add files via upload

---
+++ section08_descriptors/the_set_name.py
@@ -0,0 +1,34 @@
+class TextField:
+
+	def __init__(self, length):
+		self.length = length
+
+	def __set_name__(self, owner, name):
+		self.name = name
+
+	def __get__(self, instance, owner):
+		return instance.__dict__.get(f"TextField_{self.name}")
+
+	def __set__(self, instance, value):
+		if not type(value) == str:
+			raise TypeError("Value should be a string")
+
+		if len(value) > self.length:
+			raise ValueError(f"Value cannot exceed {self.length} characters")
+
+		instance.__dict__[f"TextField_{self.name}"] = value
+
+	def __delete__(self, instance):
+		pass
+
+
+class PersonTable:
+	first_name = TextField(200)
+	last_name = TextField(100)
+
+
+p1 = PersonTable()
+p1.first_name = "Andrew"
+p1.last_name = "Bonnie"
+print(p1.first_name)
+print(p1.last_name)
