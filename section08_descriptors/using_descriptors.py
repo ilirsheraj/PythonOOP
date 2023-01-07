62e66d652c959a8ae0512b1f5e633ef3441effbe
Like <sherajilir@gmail.com>
2023-01-07 20:06:51 +0100

    Add files via upload

--- section08_descriptors/using_descriptors.py
+++ section08_descriptors/using_descriptors.py
@@ -8,15 +8,21 @@ class TextField:
 		self.length = length
 
 	def __get__(self, instance, owner):
+		# Second step: After storing in __set__, return the value to be called by the other class below
 		return self.value
 
 	def __set__(self, instance, value):
+		# First step: set the value that you want
 		if not type(value) == str:
+			# If not string, raise type error
 			raise TypeError("Value should be a string")
 
 		if len(value) > self.length:
+			# If the length is longer than the one specified in the class below, raise value error
 			raise ValueError(f"Value cannot exceed {self.length} characters")
 
+		# If the above two conditions are fulfilled, store it
+		# Only in this step, it will not be returned
 		self.value = value
 
 	def __delete__(self, instance):
@@ -37,6 +43,6 @@ class PersonTable:
 p = PersonTable("Robbie")
 p.first_name = "Liam"
 print(p.__dict__)
-
-# we get what the descriptor provides (Liam)
+#
+# we get what the descriptor provides (Liam), because descriptor takes precedence over instance
 print(p.first_name)
